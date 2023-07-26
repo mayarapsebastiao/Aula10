3d1810916adb139131eeee50357ea80824331af3
Mayara Sebastiao <mayara.psebastiao@gmail.com>
2023-07-26 14:29:36 -0300

    aula10

---
+++ app.py
@@ -0,0 +1,11 @@
+#frutas = {"maça","laranja","abacaxi"}
+
+#frutas.add("pera")
+#frutas.remove("maça")
+#frutas.pop()
+#print(frutas)
+
+#set1 = {"maça","laranja","abacaxi"}
+#set2 ={0,3,50,-74}
+#set3 = {True,False,False,False}
+#set4 = {"Roger",34,True}
